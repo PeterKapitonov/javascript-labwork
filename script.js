5f06379552f0edd49ca73178d9c68bea28fe0c01
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:26:42 +0200

    Урок 14.Задачи на приемы работы с массивами на JavaScript

--- script.js
+++ script.js
@@ -1,55 +1,86 @@
 function task1(){
-	for(var i=1;i<10;i++){
-		for(var j=1;j<=10;j++){
-			a=String(i);
-			b=String(j);
-			document.write(a+'*'+b+'='+(i*j)+' ');
-		}
-		document.write('<br>')
+	var arr = [];
+	var x ='x';
+	for(var i=1;i<100;i++){
+		arr.push(x);
+		x+='x';
+		document.write(arr+'<br>');
 	}
 }
+
 function task2(){
-	var a = '';
-	for(var i=1;i<10;i++){
-		a+=String(i);
+	var arr = [];
+	for(var i=1;i<100;i++){
+		var num='';
+		for(var j=1;j<=i;j++){
+			num+=i;
+		}
+		arr.push(String(num));
+		arr=arr.slice(-1);
+		document.write(arr+'<br>');
 	}
-	alert(a);
 }
 
 function task3(){
-	var a = '';
-	for(var i=10;i>0;i--){
-		a+=String(i);
+	function arrayFill(x,k){
+		array=[];
+		for(var i=0;i<k;i++){
+			array.push(String(x));
+		}
+		return array;
 	}
-	alert(a);
+	alert(arrayFill('Hello',6))
+	
 }
+
 function task4(){
-	var a = '-';
-	for(var i=1;i<10;i++){
-		a+=String(i)+'-';
+	arr=[1,3,1,-6,-95,54,54,3];
+	num=1;
+	for(var i=0;i<arr.length;i++){
+		num*=arr[i];
+		alert(num);
+		if(num>10){
+			alert((i+1)+' элементa');
+			break;
+		}
 	}
-	alert(a);
+	if(num<10){
+		alert('если сложить все элементы получиться меньше 10');
+	}
+
 }
+
 function task5(){
-	var str = '';
-	for (var i = 1; i <= 20; i++) {
-		str += 'x';
-		document.write(str + '<br>');
+	var arr=[1,3,1,-6,-95,54,54,3];
+	var arr1=[];
+	for(var i=arr.length-1;i>=0;i--){
+		arr1.push(arr[i]);
 	}
+	alert(arr1);
 }
 
 function task6(){
-	for (var i = 1; i <= 129; i++) {
-		for (var j = 1; j <= i; j++) {
-			document.write(i);
+	arr=[[1, 2, 3], [4, 5], [6,3],[3,3,4]];
+	summ=0;
+	for(var i=0;i<arr.length;i++){
+		for(var j=0;j<arr[i].length;j++){
+			summ+=arr[i][j];
 		}
-		document.write('<br>');
 	}
+	alert(summ);
+
 }
+
 function task7(){
-	var str = '';
-	for(var i = 0; i <= 100; i++) {
-		str += 'xx';
-		document.write(str + '<br>');
+	var arr=[[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+	summ=0;
+	for(var i=0;i<arr.length;i++){
+		for(var j=0;j<arr[i].length;j++){
+			for(var g=0;g<arr[i][j].length;g++){
+				summ+=arr[i][j][g];
+			}
+		}
 	}
+	alert(summ);
 }
+
