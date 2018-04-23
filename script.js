88c32a232f79a24f9f5e1d8ed9cc50a4113baa6c
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:35:34 +0200

    Урок 22.Задачи на работу с таймерами в JavaScript

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
 
 function task1(){
-	var now = new Date();
-	alert(now.getDate());
+	var date=document.getElementById('task1');
+	date.innerHTML=parseInt(date.innerHTML)+1;
+	window.setTimeout(task1,1000);
 }
-function task2(){
-	var now = new Date();
-	var month_date=['январь','февраль','март','апрель','май','июнь','июль','август','сентаябр','октябрь','ноябрь','декабрь']
-	
-	alert(month_date[now.getMonth()]);
+function task2_1(){
+	window.timeId=window.setInterval(task2_2,1000);//таймер запускает функцию через каждую 1 сек
+ 	document.getElementById('task2_1').disabled = true;
+	document.getElementById('task2_3').disabled = false;
 }
-
+function task2_2(){
+	var date=document.getElementById('task2');
+	date.innerHTML=parseInt(date.innerHTML)+1;
+}
+function task2_3(){
+	clearInterval(window.timeId);//функ останавливает таймер
+	document.getElementById('task2_1').disabled = false;
+	document.getElementById('task2_3').disabled = true;
+}
+function task3_1(){
+	window.setInterval(task3,1000);
+}
 function task3(){
-	var now = new Date();
-	alert(now.getFullYear());
+	var date= new Date();
+	var hour=date.getHours();
+	var min=date.getMinutes();
+	var sec=date.getSeconds();
+	var span_task3=document.getElementById('task3');
+	span_task3.innerHTML=hour+':'+min+':'+sec;
+
 }
 
 function task4(){
-	function getZero(num){
-		if (num > 0 && num < 10) { 
-			return '0' + num;
-		} else {
-			return num;
-		}
+	var span_task4=document.getElementById('task4');
+	var taim=span_task4.innerHTML;
+	if(taim>1){
+		span_task4.innerHTML=parseInt(span_task4.innerHTML)-1;
+	}else{
+		span_task4.innerHTML='отсчёт окончен';
 	}
-	var date = new Date();
-	alert(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + getZero(date.getDate()) + '.' + getZero(date.getMonth() + 1) + '.' + date.getFullYear());
+	window.setTimeout(task4,1000);
 	
 }
 
 function task5(){
-	var date=new Date();
-	alert(date.getDay());
+	var elem=document.getElementById('task5');
+	var clas=elem.className;
+	var mass=clas.split(' ');
+	var new_mass=['task5-1','task5-2','task5-3','task5-4'];
+	for(var i=0;i<4;i++){
+		if(mass[1]==new_mass[i]){
+			var num=i;
+			break;
+		}	
+	}
+
+	if(num==3){
+		elem.className=mass[0]+' '+new_mass[0];
+
+	}else{
+		elem.className=mass[0]+' '+new_mass[num+1];
+	}
+	window.setTimeout(task5,1000);
+	
 }
 
 function task6(){
@@ -51,24 +83,22 @@ function task7(){
 }
 
 function task8(){
-	var date = new Date();
-	var minute = Math.floor(date.getTime() / (1000 * 60));
-	alert(minute);
-
+	var seconds = document.getElementById('seconds');
+	var minutes = document.getElementById('minutes');
+	var hours = document.getElementById('hours');
+	var now = new Date();
+	var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 0, 0, 0);
+	var diff =  Math.floor((midnight - now)/1000);//разница в милисекундах, перевели в секунды
+	var hoursRemain   = Math.floor(diff/(60*60));
+	var minutesRemain = Math.floor((diff-hoursRemain*60*60)/60);
+	var secondsRemain = Math.floor(diff%60);
+	hours.innerHTML   = hoursRemain;
+	minutes.innerHTML = addZero(minutesRemain);
+	seconds.innerHTML = addZero(secondsRemain); 	
+	window.setTimeout(task8,1000);	
 }
-
-function task9(){
-	var old=Date.parse('1988-01-01');
-	var new_date=new Date();
-	var diff=Math.floor((new_date-old)/(1000*60*60));
-	alert(diff);
+function addZero(num){
+	if(num <=9) return '0'+num;
+		else return num;
 }
-
-function task10(){
-	var new_date=new Date();
-	var old=new Date(new_date.getFullYear(), new_date.getMonth(), new_date.getDate(), 0, 0, 0);
-	var result = Math.floor((new_date.getTime() - old.getTime()) / 1000);
-	alert(result);
-}
-
 
