041ef7491e57dd77a6f35250145348fb373a0517
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:00:52 +0200

    Урок 6.Работа с математическими функциями в JavaScript

--- script.js
+++ script.js
@@ -1,150 +1,111 @@
-function task1while(){
-	var i = 1;
-	while(i<=100){
-		document.write(i+'<br>');
-		i++;
-	}
-}
-function task1for(){
-	for(var i=0; i<=100;i++){
-		document.write(i+'<br>');
-	}
+function task1(){
+	var a=Number(prompt('Введите а:')),b=Number(prompt('Введите b:'));
+	var ostat=a%b;
+	alert('остаток от деления а на b = '+ostat);
 }
 function task2(){
-	for(var i=11;i<=33;i++){
-		document.write(i+"<br>");
+	var a=Number(prompt('Введите а:')),b=Number(prompt('Введите b:'));
+	var ostat=a%b;
+	if(ostat==0){
+		alert('Делиться без остсатка, результат деления: '+(a/b));
+	}else{
+		alert('Делится с остатком. Остаток: '+ ostat);
 	}
 }
 
 function task3(){
-	for(var i=0;i<=100;i+=2){
-		document.write(i+"<br>");
-	}
+	var a=2,b=10;
+	var st= Math.pow(a,b);//возведение a в b-ую степень
+	alert(st);
 }
 
 function task4(){
-	var summ=0;
-	for(var i=1;i<=100;i+=1){
-		summ+=i;
-		document.write(i+' '+summ+"<br>");
-	}
-	alert(summ);
+	var st= Math.sqrt(245);//квадратный корень числа 245
+	alert(st);
 }
 
 function task5(){
-	var arr=[1,2,3,4,5];
+	var arr=[4, 2, 5, 19, 13, 0, 10];
+	var koren,summ=0;
 	for(var i=0;i<arr.length;i++){
-		alert(arr[i]);
+		summ+=arr[i]**3;
 	}
+	koren=Math.sqrt(summ);
+	document.write(summ);
 }
 
 function task6(){
-	var arr=[1,2,3,4,5];
-	var result=0;
-	for(var i=0;i<arr.length;i++){
-		result+=arr[i];
+	var num=379;
+	num1=Math.sqrt(num);
+	for(var i=0;i<20;i++){
+		document.write(num1.toFixed(i)+'<br>');
 	}
-	alert(result);
 }
 
 function task7(){
-	var obj = {green: 'зеленый', red: 'красный', blue: 'голубой'};
-	for(var key in obj){
-		alert(key); //ключи
-		alert(obj[key]); //значения
-	}
+	var num=Math.sqrt(587);
+	document.write('квадратный корень равен: '+num+'<br>');
+	document.write('Функция ceil: '+Math.ceil(num)+'<br>');
+	document.write('Функция floor: '+Math.floor(num)+'<br>');
 }
 
 function task8(){
-	var obj = {Коля: '333', Толя: '232', Сашя: '23423'};
-	for(var key in obj){
-		alert(key+'- зарплата '+obj[key]+' рублей в день.');
-	}
+	var num=[ 4, -2, 5, 19, -130, 0, 10],num_max,num_min;
+	num_max=Math.max.apply(null,num);
+	num_min=Math.min.apply(null,num);	
+	document.write(num_max+"<br>");
+	document.write(num_min);
 }
 
 function task9(){
-	var arr=[2,5,9,15,0,4];
-	for(var i=0;i<=arr.length;i++){
-		if(arr[i]>3 && arr[i]<10){
-			document.write(arr[i]+'<br>');
-		}
+	function getRandomInt(min,max){
+		return Math.floor(Math.random()*(max-min+1))+min;
 	}
+	document.write(getRandomInt(-10,100));
 
 }
 
 function task10(){
-	var arr=[-2,15,-49,-15,10,4];
-	var summ=0;
-	for(var i=0;i<=arr.length;i++){
-		if(arr[i]>0){
-			summ+=arr[i];
-		}
+	var arr=[];
+	for(var i=0;i<=20;i++){
+		arr[i]=Math.ceil(Math.random()*(100-1+1))+1;
 	}
-	document.write(arr+'<br>');
-	document.write('сумма положительных элементов равна: '+summ);
+	for(j=0;j<arr.length;j++){
+		document.write(arr[j]+'<br>');
+	}
 }
 
 function task11(){
-	var arr=[1,2,5,9,4,13,4,10];
-	for(var i=0;i<=arr.length;i++){
-		if(arr[i]==4){
-			alert('Есть!');
-			break;
-		}
-	
-	}
+	var a=Number(prompt('Введите число а:')),b=Number(prompt('Введите число b:'));
+	var modraz=Math.abs(a-b);
+	alert(modraz);
 }
 
 function task12(){
-	var arr = ['10', 20, '30', '50', '235', '3000'];
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i][0] == '1' || arr[i][0] == '2' || arr[i][0] == '5') {
-			alert(arr[i]);
-		}
-	}
+	var a=Number(prompt('Введите число а:')),b=Number(prompt('Введите число b:'));
+	var c=Math.abs(a-b);
+	alert(c);
 
 }
 
 function task13(){
-	var arr=[1,2,3,4,5,6,7,8,9];
-	var stroka='';
-		for(var i;i<=arr.length;i++){
-			stroka+='-'+String(arr[i]);
-		}
-	document.write(stroka);
+	var arr=[12, 15, 20, 25, 59, 79];
+	var sred=0;
+	for(var i=0;i<arr.length;i++){
+		sred+=arr[i];
+	}
+	alert(sred+' '+arr.length);
+	sred=sred/arr.length;
+	alert(sred);
 }
 
 function task14(){
-	var arr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-	for(var i=0;i<arr.length;i++){
-		if(arr[i]=='Сб' || arr[i]=='Вс'){
-			document.write('<strong>'+arr[i]+'</strong><br>');
-		}else{
-			document.write(arr[i]+'<br>');
-		}
+	var a=Number(prompt('Введите число а:'));
+	fact=1;
+	for(var i=1;i<=a;i++){
+		fact*=i;
 	}
+	document.write(a+'!='+fact);
 }
 
-function task15(){
-	var arr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-	var day=Number(prompt('Введите номер дня недели:'));
-	for(var i=0;i<arr.length;i++){
-		if((i+1)==day){
-			document.write('<i>'+arr[i]+'</i><br>');
-		}else{
-			document.write(arr[i]+'<br>');
-		}
-	}
-}
-
-function task16(){
-	var num = Number(prompt('Ведите число:'));
-	for(var i=0;i<100;i++){
-		num/=2;
-		if(num<50){
-			break;
-		}
-	}
-	alert(num);
-}
 
