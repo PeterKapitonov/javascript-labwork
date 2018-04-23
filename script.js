70760ff9041b6b98d9ae48b1490cef0a20195b0a
PeterKapitonov <79373925842@mail.ru>
2018-04-23 21:55:08 +0200

    Урок 2.Основы языка JavaScript

---
+++ script.js
@@ -0,0 +1,120 @@
+function task1(){
+	var num = 3;
+	alert(num);
+}
+
+function task2(){
+	var a = 10, b=2;
+	summ = a+b;
+	raz = a-b;
+	proiz = a*b;
+	chast= a/b;
+
+	alert(a+"+"+b+"="+summ);
+	alert(a+"-"+b+"="+raz);
+	alert(a+"*"+b+"="+proiz);
+	alert(a+"/"+b+"="+chast);
+}
+
+function task3(){
+	var c = 15;
+	var d = 2;
+	var result = c + d;
+	alert(result);
+}
+
+function task4(){
+	var a = 10;
+	var d = 2;
+	var c = 5;
+	alert(a + b + c);
+}
+
+function task5(){
+	var a = 17;
+	var b = 10;
+	var c = a - b;
+	var d = 7;
+	var result = c + d;
+	alert(result);
+}
+
+function task6(){
+	var str = 'Привет, Мир!';
+	alert(str);
+}
+
+function task7(){
+	var str1 = 'Привет, ';
+	var str2 = 'Мир!';
+	alert(str1 + str2);
+}
+
+function task8(){
+	var name = 'Евгений';
+	alert('Привет, ' + name);
+}
+
+function task9(){
+	var age = 19;
+	alert("мне"+age+"лет");
+}
+
+function task10(){
+	var youname = prompt("Ваше имя?");
+	alert("Очень приятно,"+youname);
+}
+
+function task11(){
+	var number= prompt("Введите число:");
+	alert("квадрат вашего числа: "+number**2);
+}
+
+function task12(){
+	var str ="abcde";
+	a=str[0];
+	c=str[2];
+	e=str[4];
+	alert(a);
+	alert(c);
+	alert(e);
+}
+
+function task13(){
+	var num = "1234567";
+	alert(num[0]*num[1]*num[2]*num[3]*num[4]*num[5]*num[6])
+}
+
+function task14(){
+	var hour = 60*60, day = 60*60*24, year = 60*60*24*365;
+	alert(hour);
+	alert(day);
+	alert(year);
+}
+
+function task15(){
+	var hour=10, min =02,sec = 35;
+	alert(hour +":"+min+":"+sec);
+}
+
+function task16(){
+	var num = 13;
+	alert(num **2);
+}
+
+function task17(){
+	var num = 17;
+	num = num + 27;
+	num = num - 22;
+	num = num * 210;
+	num = num / 15;
+	alert(num);
+}
+
+function task18(){
+	var num = 28;
+	num = num + 3;
+	num = num + 2;
+	num = num - 22;
+	alert(num);
+}
