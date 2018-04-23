f752d4abef92519505c56e23a15e00c71f5440d2
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:19:11 +0200

    Урок 9.Отработка стандартных функций JavaScript

--- script.js
+++ script.js
@@ -1,82 +1,35 @@
 function task1(){
-	var arr1=[1,2,3];
-	var arr2=[4,5,6];
-	alert(arr1.concat(arr2));
+	var arr='hello world!';
+	var arr1=arr[0].toUpperCase()+arr.substr(1);
+	alert(arr1);
 }
 function task2(){
-	var arr=[1,2,3,3,4];
-	arr.reverse();
-	alert(arr);
-}
-
-function task3(){
-	var arr=[1,2,3];
-	arr.push(4,4,5,6);
-	alert(arr);
-}
-function task4(){
-	var arr=[1,2,3];
-	arr.unshift(4,4,5,6);
-	alert(arr);
-}
-
-function task5(){
-	var arr= ['js', 'css', 'jq'];
-	alert(arr.shift());
-}
-
-function task6(){
-	var arr= ['js', 'css', 'jq'];
-	alert(arr.pop());
-}
-
-function task7(){
-	var arr=[1, 2, 3, 4, 5];
-	var arr1=arr.slice(0,3);
-	alert(arr1);
-}
-
-function task8(){
-	var arr=[1, 2, 3, 4, 5];
-	var arr1=arr.slice(3);
-	alert(arr1);
-}
-
-function task9(){
-	var arr=[1, 2, 3, 4, 5];
-	arr.splice(1,2);
-	alert(arr);
-}
-
-function task10(){
-	var arr=[1, 2, 3, 4, 5];
-	var arr1=arr.splice(1,3);
-	alert(arr1);
-}
-
-function task11(){
-	var arr = [1, 2, 3, 4, 5];
-	arr.splice(3, 0, 'a', 'b', 'c');
-	alert(arr);
-}
-
-function task12(){
-	var arr = [1, 2, 3, 4, 5];
-	arr.splice(1, 0, 'a', 'b');
-	arr.splice(6, 0, 'c');
-	arr.splice(8, 0, 'e');
-	alert(arr);
-}
-function task13(){
-	var arr = [3, 4, 1, 2, 7];
-	arr.sort();
-	alert(arr);
-}
-
-function task14(){
-	var arr = {js: 'test', jq: 'hello', css: 'world'};
-	var result = Object.keys(arr);
+	var str = '123456';
+	result = str.split('').reverse().join('');
 	alert(result);
 }
 
+function task3(){
+	var str = '3751';
+	arr = str.split('');
+	arr.sort();
+	var result = arr.join('');
+	alert(result);
+}
+function task4(){
+	var str = 'http://phphtml';
+	if (str.substr(0, 7) == 'http://') {
+		alert('Да');
+	} else {
+		alert('Нет');
+	}	
+}
 
+function task5(){
+	var str = 'index.html';
+	if (str.substr(-5) == '.html') {
+		alert('Да');
+	} else {
+		alert('Нет');
+	}
+}
