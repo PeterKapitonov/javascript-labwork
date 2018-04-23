28b12a42a1d7bab010ba24a12f7dbfd6f2f9c3e7
PeterKapitonov <79373925842@mail.ru>
2018-04-23 21:59:46 +0200

    Урок 5.Работа с циклами for and while in JavaScript

--- script.js
+++ script.js
@@ -1,189 +1,150 @@
-function task1(){
-	var a=Number(prompt('Введите число:'));
-	if(a==0){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+function task1while(){
+	var i = 1;
+	while(i<=100){
+		document.write(i+'<br>');
+		i++;
 	}
 }
-
+function task1for(){
+	for(var i=0; i<=100;i++){
+		document.write(i+'<br>');
+	}
+}
 function task2(){
-	var a=Number(prompt('Введите число:'));
-	if(a>0){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	for(var i=11;i<=33;i++){
+		document.write(i+"<br>");
 	}
 }
 
 function task3(){
-	var a=Number(prompt('Введите число:'));
-	if(a<0){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	for(var i=0;i<=100;i+=2){
+		document.write(i+"<br>");
 	}
 }
 
 function task4(){
-	var a=Number(prompt('Введите число:'));
-	if(a>=0){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	var summ=0;
+	for(var i=1;i<=100;i+=1){
+		summ+=i;
+		document.write(i+' '+summ+"<br>");
 	}
-
+	alert(summ);
 }
 
 function task5(){
-	var a=Number(prompt('Введите число:'));
-	if(a<=0){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	var arr=[1,2,3,4,5];
+	for(var i=0;i<arr.length;i++){
+		alert(arr[i]);
 	}
 }
 
 function task6(){
-	var a=Number(prompt('Введите число:'));
-	if(a!=0){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	var arr=[1,2,3,4,5];
+	var result=0;
+	for(var i=0;i<arr.length;i++){
+		result+=arr[i];
 	}
+	alert(result);
 }
 
 function task7(){
-	var a=prompt('Введите число:');
-	if(a=='test' || a=='тест'){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	var obj = {green: 'зеленый', red: 'красный', blue: 'голубой'};
+	for(var key in obj){
+		alert(key); //ключи
+		alert(obj[key]); //значения
 	}
 }
 
 function task8(){
-	var a=prompt('Введите число:');
-	if(a==='1'){
-		alert('Верно!');
-	}else{
-		alert( 'Неверно');
+	var obj = {Коля: '333', Толя: '232', Сашя: '23423'};
+	for(var key in obj){
+		alert(key+'- зарплата '+obj[key]+' рублей в день.');
 	}
 }
 
 function task9(){
-	var test  = true;
-	if (test) {
-		alert('Верно');
-	} else {
-		alert('Неверно');
+	var arr=[2,5,9,15,0,4];
+	for(var i=0;i<=arr.length;i++){
+		if(arr[i]>3 && arr[i]<10){
+			document.write(arr[i]+'<br>');
+		}
 	}
+
 }
 
 function task10(){
-	var test  = true;
-	if (test!=true) {
-		alert('Верно');
-	} else {
-		alert('Неверно');
+	var arr=[-2,15,-49,-15,10,4];
+	var summ=0;
+	for(var i=0;i<=arr.length;i++){
+		if(arr[i]>0){
+			summ+=arr[i];
+		}
 	}
+	document.write(arr+'<br>');
+	document.write('сумма положительных элементов равна: '+summ);
 }
 
 function task11(){
-	var a=Number(prompt('Введите число:'));
-	if(0<a && a<5){
-		alert('Верно');
-	} else {
-		alert('Неверно');
+	var arr=[1,2,5,9,4,13,4,10];
+	for(var i=0;i<=arr.length;i++){
+		if(arr[i]==4){
+			alert('Есть!');
+			break;
+		}
+	
 	}
 }
 
 function task12(){
-	var a=Number(prompt('Введите число:'));
-	if(a==0 || a==2){
-		a+=7;
-	}else{
-		a=a/10;
+	var arr = ['10', 20, '30', '50', '235', '3000'];
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i][0] == '1' || arr[i][0] == '2' || arr[i][0] == '5') {
+			alert(arr[i]);
+		}
 	}
-	alert(a);
+
 }
 
 function task13(){
-	var a=1, b=2;
-	if(a<=1 && b>=3){
-		a=a+b;
-	}else{
-		a=a-b;
-	}
-	alert(a);
+	var arr=[1,2,3,4,5,6,7,8,9];
+	var stroka='';
+		for(var i;i<=arr.length;i++){
+			stroka+='-'+String(arr[i]);
+		}
+	document.write(stroka);
 }
 
 function task14(){
-	var a=Number(prompt('Введите число a:')), b=Number(prompt('Введите число b:'));
-	if(a>2 && a<11 && b>=5  && b<14){
-		alert('Вверно');
-	}else{
-		alert('Неверно!');
+	var arr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+	for(var i=0;i<arr.length;i++){
+		if(arr[i]=='Сб' || arr[i]=='Вс'){
+			document.write('<strong>'+arr[i]+'</strong><br>');
+		}else{
+			document.write(arr[i]+'<br>');
+		}
 	}
-	
 }
 
 function task15(){
-	var num =Number(prompt('Введите число от 0 до 4:'));
-	switch(num){
-		case 1:
-			result = 'Зима';
-		break;
-		case 2:
-			result = 'Весна';
-		break;
-		case 3:
-			result = 'Лето';
-		break;
-		case 4:
-			result = 'Осень';
-		break;
+	var arr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+	var day=Number(prompt('Введите номер дня недели:'));
+	for(var i=0;i<arr.length;i++){
+		if((i+1)==day){
+			document.write('<i>'+arr[i]+'</i><br>');
+		}else{
+			document.write(arr[i]+'<br>');
+		}
 	}
-	alert(result);
 }
 
 function task16(){
-	var day =Number(prompt('Введите число от 0 до 31:'));
-	if(0<day && day<=10){
-		var decada=1;
+	var num = Number(prompt('Ведите число:'));
+	for(var i=0;i<100;i++){
+		num/=2;
+		if(num<50){
+			break;
+		}
 	}
-	if(10<day && day<=20){
-		var decada=2;
-	}
-	if(20<day && day<=31){
-		var decada=3;
-	}
-	alert(decada);
+	alert(num);
 }
 
-function task17(){
-	var month =Number(prompt('Введите число от 0 до 12:'));
-	if (month == 12 || month <= 2) {
-		var result = 'Зима';
-	}
-	if (month >= 3 && month <= 5) {
-		result = 'Весна';
-	}
-	if (month >= 6 && month <= 8) {
-		result = 'Лето';
-	}
-	if (month >= 9 && month <= 11) {
-		result = 'Осень';
-	}
-	alert(result);
-}
-	
-function task18(){
-	var stroka =prompt('Введите abcde');
-	s=stroka[0];
-	if(s=='a'){
-		alert('да');
-	}else{
-		alert('нет');
-	}
-}
