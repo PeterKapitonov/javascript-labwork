bf88cfdcc867ecace1a79d6bb3005186ffc8a595
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:28:23 +0200

    Урок 16.Практика на работу с пользовательскими функциями

--- script.js
+++ script.js
@@ -1,94 +1,56 @@
 function task1(){
-	function isNumberInRange(n){
-		if(n>0 && n<10){
-			return true;
-		}else{
-			return false;
+	function ucfirst(stroka){
+		var sekta=stroka.split(' ');
+		var new_stroka='';
+		for(var i=0;i<sekta.length;i++){
+			new_stroka+=sekta[i][0].toUpperCase()+sekta[i].slice(1)+' ';
 		}
+		return new_stroka;
 	}
-	var a=prompt('Введите число:');
-	alert(isNumberInRange(a));
+	alert(ucfirst('hello world! good morning'));
 }
 
 function task2(){
-	function isNumberInRange(n){
-		if(n>0 && n<10){
-			return true;
-		}else{
-			return false;
+	function ucfirst(stroka){
+		var sekta=stroka.split('_');
+		var new_stroka=sekta[0]+'_';
+		for(var i=1;i<sekta.length;i++){
+			if(i<sekta.length-1){
+				new_stroka+=sekta[i][0].toUpperCase()+sekta[i].slice(1)+'_';
+			}else{
+				new_stroka+=sekta[i][0].toUpperCase()+sekta[i].slice(1);
+			}
+
 		}
+		return new_stroka;
 	}
-	var arr = [1,23,23,2,6,2,4,7,22];
-	var arr1=[];
-	for(var i=0;i<arr.length;i++){
-		if(isNumberInRange(arr[i])==true){
-			arr1.push(arr[i]);
-		}
-	}
-	alert(arr1);
+	alert(ucfirst('var_text_hello'));
 }
 
 function task3(){
-	function getDigitsSum(т){
-		var m=String(m);
-		var summ=0;
-		for(var i=0;i<m.length;i++){
-			summ+=Number(m[i]);
-		}
-		return summ;
+	function inArray(text,arr){
+		for(var i=0;i<arr.length;i++){
+			if(arr[i]==text){
+				return true;
+			}else{
+				return false;
+			}
+		}	
 	}
-	m=getDigitsSum(1234234234);
-	alert(m);
+	var arr=['hello','world','!'];
+	alert(inArray('hello',arr));
 }
 
 function task4(){
-	arr=[1,3,1,-6,-95,54,54,3];
-	num=1;
-	for(var i=0;i<arr.length;i++){
-		num*=arr[i];
-		alert(num);
-		if(num>10){
-			alert((i+1)+' элементa');
-			break;
+	function inArray(num){
+		num_arr=num.split('');
+		var num='';
+		for(var i=0;i<num_arr.length;i+=2){
+			num+=num_arr[i+1]+num_arr[i];
 		}
+		return num;
 	}
-	if(num<10){
-		alert('если сложить все элементы получиться меньше 10');
-	}
+	alert(inArray('123456'));
 
-}
-
-function task5(){
-	var arr=[1,3,1,-6,-95,54,54,3];
-	var arr1=[];
-	for(var i=arr.length-1;i>=0;i--){
-		arr1.push(arr[i]);
-	}
-	alert(arr1);
-}
-
-function task6(){
-	arr=[[1, 2, 3], [4, 5], [6,3],[3,3,4]];
-	summ=0;
-	for(var i=0;i<arr.length;i++){
-		for(var j=0;j<arr[i].length;j++){
-			summ+=arr[i][j];
-		}
-	}
-	alert(summ);
-
-}
-
-function task7(){
-	var arr=[[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
-	summ=0;
-	for(var i=0;i<arr.length;i++){
-		for(var j=0;j<arr[i].length;j++){
-			for(var g=0;g<arr[i][j].length;g++){
-				summ+=arr[i][j][g];
-			}
-		}
-	}
-	alert(summ);
 }
 
