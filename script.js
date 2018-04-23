a5d262426d1bcd33ab91b702f6dfec0bc7ec02ae
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:21:48 +0200

    Урок 12.Приемы работы с логическими значениями

--- script.js
+++ script.js
@@ -1,48 +1,33 @@
 function task1(){
-	arr=[1,23,324,23,3,2,23,5,3,5,32,24,24];
-	for(var i=0;i<arr.length;i++){
-		if(arr[i]==5){
-			var a=1;
-			break;
+	function sum(a,b){
+		if(a==b){
+			return true;
+		}else{
+			return false;
 		}
-		
 	}
-	if (a==1){
-		alert('yes');
-	}else{
-		alert('no');
-	}
+	alert(sum(1,1));
 }
 function task2(){
-	var a=Number(prompt('Введите число:'));
-	flag = 0;
-	for(var i=2;i<a;i++){
-		if(a%i==0){
-			flag = true
-			break;
+	function sum(a,b){
+		if((a+b)>10){
+			return true;
+		}else{
+			return false;
 		}
 	}
-	if(flag == true){
-		alert('true');
-	}else{
-		alert('false');
-	}
+	alert(sum(1,11));
 
 }
 
 function task3(){
-	var arr=[1,2,13,13,4,32];
-	flag = 0;
-	for(var i=1;i<arr.length;i++){
-		if(arr[i]==arr[i-1]){
-			flag = true
-			break;
+	function num(a){
+		if(a<0){
+			return true;
+		}else{
+			return false;
 		}
 	}
-	if(flag == true){
-		alert('ДА');
-	}else{
-		alert('НЕТ');
-	}
+	alert(num(1));
 }
 
