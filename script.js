88993ac2f78dd441db7026a5593b3d229c170f54
PeterKapitonov <79373925842@mail.ru>
2018-04-23 22:26:00 +0200

    Урок 13.Задачи на приемы работы с циклами на JavaScript

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 function task1(){
-	function sum(a,b){
-		if(a==b){
-			return true;
-		}else{
-			return false;
+	for(var i=1;i<10;i++){
+		for(var j=1;j<=10;j++){
+			a=String(i);
+			b=String(j);
+			document.write(a+'*'+b+'='+(i*j)+' ');
 		}
+		document.write('<br>')
 	}
-	alert(sum(1,1));
 }
 function task2(){
-	function sum(a,b){
-		if((a+b)>10){
-			return true;
-		}else{
-			return false;
-		}
+	var a = '';
+	for(var i=1;i<10;i++){
+		a+=String(i);
 	}
-	alert(sum(1,11));
-
+	alert(a);
 }
 
 function task3(){
-	function num(a){
-		if(a<0){
-			return true;
-		}else{
-			return false;
-		}
+	var a = '';
+	for(var i=10;i>0;i--){
+		a+=String(i);
 	}
-	alert(num(1));
+	alert(a);
+}
+function task4(){
+	var a = '-';
+	for(var i=1;i<10;i++){
+		a+=String(i)+'-';
+	}
+	alert(a);
+}
+function task5(){
+	var str = '';
+	for (var i = 1; i <= 20; i++) {
+		str += 'x';
+		document.write(str + '<br>');
+	}
 }
 
+function task6(){
+	for (var i = 1; i <= 129; i++) {
+		for (var j = 1; j <= i; j++) {
+			document.write(i);
+		}
+		document.write('<br>');
+	}
+}
+function task7(){
+	var str = '';
+	for(var i = 0; i <= 100; i++) {
+		str += 'xx';
+		document.write(str + '<br>');
+	}
+}
