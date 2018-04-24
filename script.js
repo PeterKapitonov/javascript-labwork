e13b65c4ec1a0a28b94366816be70c303bb3253e
PeterKapitonov <79373925842@mail.ru>
2018-04-25 00:12:11 +0200

    Урок 26

--- script.js
+++ script.js
@@ -1,60 +1,13 @@
 function task1(){
-	var elem = document.getElementById('task1');
-	alert(elem.clientTop);
-	
+	alert(document.documentElement.clientHeight);
 }
 function task2(){
-	var elem = document.getElementById('task2');
-	alert('clientLeft:  '+elem.clientLeft);
-	alert('clientRight:  '+elem.clientRight);
+	var heightWindow = document.body.clientHeight;
+	window.scrollTo(0, heightWindow);
 }
 
 function task3(){
-	var elem = document.getElementById('task3');
-	alert('ширина окна:  '+elem.offsetWidth);
+	alert('Текущая прокрутка сверху: '+pageYOffset);
 	
 }
 
-function task4(){
-	var elem = document.getElementById('task4');
-	alert('высота окна:  '+elem.offsetHeight);
-}
-
-function task5(){
-	var elem = document.getElementById('task5');
-	alert('ширина без границы:  '+elem.clientWidth);
-}
-
-function task6(){
-	var elem = document.getElementById('task6');
-	alert('высота без границы:  '+elem.clientHeight); 
-}
-
-function task7(){
-	var elem = document.getElementById('task7');
-	alert('высота без границы и без padding:  '+getComputedStyle(elem).height+'  ширина без границы и без padding:  '+getComputedStyle(elem).width); 
-}
-
-function task8(){
-	var elem = document.getElementById('task8');
-	alert('элемент по вертикали прокручен на '+elem.scrollTop+'px'); 
-}
-
-function task9(){
-	var elem = document.getElementById('task9');
-	alert('элемент по горизонтали прокручен на '+elem.scrollLeft+'px'); 
-}
-
-function task10(){
-	var elem = document.getElementById('task10');
-	elem.scrollTop=100; 
-}
-
-function task11(){
-	var elem = document.getElementById('task11');
-	elem.scrollLeft=100;
-}
-
-function task12(){
-	window.scrollTo(0, document.body.clientHeight);
-}
