8b646de6e12f63ebc687616eaf29f8a8b503e037
PeterKapitonov <79373925842@mail.ru>
2018-04-25 00:09:39 +0200

    Урок 24.Задачи на продвинутую работу с DOM на JavaScript

--- script.js
+++ script.js
@@ -1,53 +1,102 @@
-
 function task1(){
-	var elems = document.querySelectorAll('.task11');
-	for (var i = 0; i < elems.length; i++) {
-		elems[i].onclick = func11;
-	}
-}
-function func11() {
-	alert(this.getAttribute('class'));
+	var elem = document.getElementById('task1');
+	alert(elem.className); 
+	elem.classList.add('www'); 
+	alert(elem.className); 
 }
 function task2(){
-	var elems = document.getElementsByTagName('a');
-	for (var i = 0; i < elems.length; i++) {
-		elems[i].addEventListener('mouseover', func22);
-	}
+	var elem = document.getElementById('task2');
+	alert(elem.className); 
+	elem.classList.remove('www'); 
+	alert(elem.className); 
 }
-function func22() {
-	this.title = this.innerHTML;
-}
+
 function task3(){
-	var elems = document.getElementsByTagName('a');
-	for (var i = 0; i < elems.length; i++) {
-		elems[i].addEventListener('mouseover', func33);
-	}
-}
-function func33() {
-	this.title = this.innerHTML+ ' (' + this.href + ')';
+	var elem = document.getElementById('task3');
+	alert(elem.className); 
+	var elem1=elem.classList.contains('www'); 
+	alert(elem1); 
+	
 }
 
 function task4(){
-	var elems = document.getElementsByTagName('a');
-	for (var i = 0; i < elems.length; i++) {
-		elems[i].addEventListener('mouseover', func44);
-	}
-	
-}
-function func44() {
-	this.title = this.innerHTML+ ' (' + this.href + ')';
-	
-}
-var parag = document.getElementById('test5');
-function task5(){
-	var elems = document.querySelectorAll('.task55');
-	for (var i = 0; i < elems.length; i++) {
-		elems[i].addEventListener('blur', func55);
-	}
-}
-function func55() {
-	parag.innerHTML = this.value;
-	
+	var elem = document.getElementById('task4');
+	elem.classList.toggle('www');  
 }
 
+function task5(){
+	var elem = document.getElementById('task5');
+	alert(elem.className); 
+	var elem1=elem.classList.length; 
+	alert('количество классов:'+elem1); 
+}
+
+function task6(){
+	var elem = document.getElementById('task6');
+	var elem_len=elem.classList.length; 
+	for (var i = 0; i < elem.classList.length; i++) {
+		alert(elem.classList[i]);
+ 	}   
+}
+
+function task7(){
+	var elem = document.getElementById('task7');
+	elem.style.cssText = 'color: red; font-size: 30px; border: solid 1px black;';  
+}
+
+function task8(elem){
+	alert(elem.tagName);
+}
+
+function task9(){
+	var elem = document.getElementById('task9');
+	alert(elem.tagName.toLowerCase());
+}
+
+function task10(){
+	var elems = document.querySelectorAll('.www10');
+	for (var i = 0; i < elems.length; i++) {
+		elems[i].innerHTML = elems[i].innerHTML + elems[i].tagName.toLowerCase();
+	}	
+}
+
+function task11(){
+	var arr = {
+	'ru':['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+};
+	alert(arr['ru'][0]);
+	alert(arr['en'][2]);
+}
+
+function task12(){
+	var arr = {
+	'ru':['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+};
+	var lang = 'ru';
+	var day = 3;
+	alert(arr[lang][day]);
+}
+function task13(){
+	var arr = ['a','b','c'];
+	alert(arr[0]);
+	alert(arr[1]);
+	alert(arr[2]);
+}
+function task14(){
+	var arr = ['a','b','c'];
+	alert(arr[0]);
+	alert(arr[1]);
+	alert(arr[2]);
+}
+
+function task15(){
+	var arr = ['a', 'b', 'c', 'd','e','f'] ;
+	alert(arr[0]+arr[1]);
+	alert(arr[2]+arr[3]);
+	alert(arr[4]+arr[5]);
+	alert(arr[0]+'+'+arr[1]+','+arr[2]+'+'+arr[3]+','+arr[4]+'+'+arr[5]);
+	
+}
 
