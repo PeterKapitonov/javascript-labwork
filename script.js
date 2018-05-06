8944335a847017ac5c15db1e775c1b67563074e6
PeterKapitonov <79373925842@mail.ru>
2018-05-06 19:55:19 +0200

    вкладки 2

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-		var tabs = document.getElementById('tabs');
-		createTabs(tabs);
-		function createTabs(root) {
-			var items = root.getElementsByClassName('tabs__item');
-			var texts = root.getElementsByClassName('tabs__text');
-			root.addEventListener('click', go);
-			function go(event) {
-				var targ = event.target;
-				if (targ.tagName !== 'LI') return;
-				var select = 'tabs__item-select';
-				for (var i = 0; i < items.length; i++) {
-					items[i].classList.remove('select');
-				}
-				targ.classList.add('select');
-				for (var i = 0; i < texts.length; i++) {
-					texts[i].style.display = 'none'
-				} 
-				var id = targ.getAttribute('data-id');;
-				root.querySelector('div[id=' + id + ']').style.display = 'block';
-			}
-		}
+var accordion = document.getElementById('accordion');
+accordion.addEventListener('click', change);
+function change(event) {
+	var targ = event.target;
+	if (targ.tagName !== 'H3') return; 
+	if (targ.classList.contains('select')) {
+		hideAll();
+	} else {
+		hideAll();
+		targ.classList.add('select');
+		showText(targ.nextElementSibling);
+	}
+}
+function hideAll() {
+	var h3El = accordion.querySelectorAll('h3');
+	var divEl = accordion.querySelectorAll('div');
+	for (var i = 0; i < h3El.length; i++) {
+		h3El[i].classList.remove('select');
+	}
+	for (var i = 0; i < divEl.length; i++) {
+		divEl[i].style.height = '0';
+	}
+}
+function showText(textEl) {
+	textEl.style.height = textEl.scrollHeight + 'px';
+}
