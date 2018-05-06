1d2bdd7036077a07dce9737c5208b97876ee5114
PeterKapitonov <79373925842@mail.ru>
2018-05-06 19:54:30 +0200

    вкладки 1

--- script.js
+++ script.js
@@ -1,58 +1,21 @@
-var daysEl = document.getElementById('days');
-		var dateEl = document.getElementById('date');
-		var prevEl = document.getElementById('prev');
-		var nextEl = document.getElementById('next');
-		var deferentMonth = 0;
-		var oneHour = 1000 * 60 * 60;
-		var oneDay = oneHour * 24;
-		var MONTHS = [
-			'Январь','Февраль','Март','Апрель', 
-			'Май','Июнь','Июль','Август',
-			'Сентябрь','Октябрь','Ноябрь','Декабрь'
-		];
-		create();
-		prevEl.addEventListener('click', prev);
-		nextEl.addEventListener('click', next);
-		function prev() {
-			deferentMonth--;
-			create();
-		}
-		function next() {
-			deferentMonth++;
-			create();
-		}
-		function create() {
-			var d = new Date();	
-			d.setMonth(d.getMonth() + deferentMonth);
-			var year = d.getFullYear();
-			var month = d.getMonth();
-			var dayCount = (new Date(year, month + 1, 0)).getDate();
-			var dayStart = (new Date(year, month, 1)).getDay();
-			if (dayStart === 0) {
-				dayStart = 7;
-			}
-			var today = null;
-			if (deferentMonth === 0) {
-				var today = d.getDate();
-			}
-			printDate(d);
-			generate(dayCount, today, dayStart);
-		}
-		function generate(count, today, start) {
-			var html = '';
-			for (var i = 1; i < start; i++) {
-				html += '<div class="day other"></div>';
-			}
-			for (var i = 1; i <= count; i++) {
-				var cl = 'day';
-				if (i === today) {
-					cl += ' today';
+		var tabs = document.getElementById('tabs');
+		createTabs(tabs);
+		function createTabs(root) {
+			var items = root.getElementsByClassName('tabs__item');
+			var texts = root.getElementsByClassName('tabs__text');
+			root.addEventListener('click', go);
+			function go(event) {
+				var targ = event.target;
+				if (targ.tagName !== 'LI') return;
+				var select = 'tabs__item-select';
+				for (var i = 0; i < items.length; i++) {
+					items[i].classList.remove('select');
 				}
-				html += '<div class="' + cl + '">' + i + '</div>';
+				targ.classList.add('select');
+				for (var i = 0; i < texts.length; i++) {
+					texts[i].style.display = 'none'
+				} 
+				var id = targ.getAttribute('data-id');;
+				root.querySelector('div[id=' + id + ']').style.display = 'block';
 			}
-			daysEl.innerHTML = html;
-		}
-		function printDate(d) {
-			dateEl.innerHTML = d.getFullYear() + ' ' 
-				+ MONTHS[d.getMonth()];
 		}
