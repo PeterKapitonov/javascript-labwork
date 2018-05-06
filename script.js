67c14cac4edaf9235c5f1d60fad9b6b787ab7dce
PeterKapitonov <79373925842@mail.ru>
2018-05-06 20:01:34 +0200

    Урок 33.Задачи на некоторые продвинутые функции JavaScript

--- script.js
+++ script.js
@@ -1,166 +1,77 @@
 
 function task1(){
-	var a=prompt();
-	if (a > 0) {
-		var ggg = function() {
-			alert('!');
-		}
-	} else {
-		var ggg = function() {
-			alert('!!');
-		}
-	}
-	ggg();	
+	var arr = [5, 6, 7, 8, 9];
+	var newArr = [];
+	arr.forEach(function(elem) {
+		newArr.push(elem * elem);
+	});
+	alert(newArr)  
 } 
 
 function task2(){
-	var ggg = function(n, f) {return f(n);}
-	var funcNew = function(a){return Math.pow(a, 2)};
-	num = 2;
-	alert(ggg(num, funcNew));
+	var arr = [5, 6, 7, 8, 9];
+	var sum = 0;
+	arr.forEach(function(elem) {
+		sum += elem;
+	});
+	alert(sum)    
 } 
 
 
 function task3(){
-	var ggg = function(a, b){return a() + b();};
-	var f1 = function(){
-		return 3
-	}
-	var f2 = function(){
-		return 4
-	}
-	alert(ggg(f1, f2));
+	var arr = [5, 6, 7, 8, 9];
+	var newArr = arr.map(function(elem) {
+		return elem * elem;
+	});
+	document.write(newArr);   
 
 } 
 
 function task4(){
-	var ggg = function(a, b, c){return b(a) + c(a);};
-	var square = function(n){return Math.pow(n, 2)};
-	var cub = function(n){return Math.pow(n, 3)};
-	alert(ggg(3, square, cub));
+	var arr = [5, 6, 7, -8, 9];
+	function func(elem){return elem > 0};
+	alert(arr.every(func));  
 
 } 
 
 function task5(){
-	var each = function(startArr, f){return f(startArr)};
-	var arr = [64, 49, 36, 25, 16];
-	var myFunc = function(a){
-		var newArr = [];
-		for (var i = 0; i < a.length; i++) {
-			newArr[i]=Math.sqrt(a[i]);
-		}
-			return newArr;
-	}	
-
-	alert(each(arr, myFunc));
+	var arr = [5, 6, -7, 8, 9];
+	function func(elem){return elem < 0};
+	alert(arr.some(func));  
 } 
 
 function task6(){
-	var each = function(numArr, funcArray) {
-		var newArr = [];
-		for (var i = 0; i < numArr.length; i++){
-			var current = funcArray[i % funcArray.length](numArr[i]);
-			newArr.push(current);
-  		}
-		return newArr
-	}
-
-	var f1 = function(a){return a + 3};
-	var f2 = function(a){return a + 2};
-	var f3 = function(a){return a + 1};
-	var funcAll = [f1, f2, f3];
-	var startArray = [0,1,2, 3, 4, 5, 6, 7, 8, 9,10, 11];
-	document.write(each(startArray, funcAll));
+	var arr = [1, -3, 5, 6, -7, 8, 9, -11];
+	function func(elem){return elem < 0};
+	var newArr = arr.filter(func);
+	alert(newArr);  
 } 
 
 
 function task7(){
-	function counter() {
-		var i = 1;
-		return function() {return i++};
-	}
-
-	var func = counter();
-
-	alert(func());
-	alert(func());
-	alert(func());
-	alert(func());
-	alert(func());
-	alert(func());
+	var arr = [1, -3, 5, 6, -7, 8, 9, -11];
+	function func(elem){return elem % 2 == 0};
+	var newArr = arr.filter(func);
+	alert(newArr); 
 }
 
-var f1 = function(){   //task8 ----------------------------------------
-	var k = 1;
-	return function() {
-		this.innerHTML = k;
-		return k++;
-	}
+function task8(){
+	var arr = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
+	function func(elem){return elem.length > 5};
+	var newArr = arr.filter(func);
+	alert(newArr);  
 }
 
-var all_buttons = document.getElementsByClassName('button8');
-for (var i = 0; i < all_buttons.length; i++) {
-	all_buttons[i].addEventListener('click', f1(this));
+function task9(){
+	var arr = [1, 'string', [3, 4], 5, [6, 7]];
+	function func(elem){return Array.isArray(elem)};
+	var newArr = arr.filter(func);
+	alert(newArr);  
 }
 
-
-
-
-//task9 ----------------------------------------
-
-
-var colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-var p_collect = document.getElementsByTagName('p');
-
-var f1 = function() {
-	var k = 0;
-	return function() {
-		this.style.color = colors[k];
-		k++;
-		if (k == colors.length){k=0};
-	}
+function task10(){
+	var arr = [5, -3, 6, -5, 0, -7, 8, 9];
+	function func(elem){return elem < 0};
+	var newArr = arr.filter(func);
+	alert(newArr.length);  
 }
-
-for (var i = 0; i < p_collect.length; i++) {
-	p_collect[i].addEventListener('click', f1());
-}
-
-
-
-//task10  ----------------------------------------
-
-var f1 = function(){
-	var a = 0;
-	var b = 1;
-	var temp;
-	return function() {
-		this.nextElementSibling.innerHTML = a;
-		temp = a;
-		a = b;
-		b = temp +a;
-	}
-}
-
-var buts_collect = document.getElementsByClassName('button10');
-for (var i = 0; i < buts_collect.length; i++) {
-	buts_collect[i].addEventListener('click', f1());
-}
-
-//task12 ----------------------------------------
-
-var f12 = function() {
-	var already_used = [];
-	return function() {
-		var new_num = Math.floor(Math.random() * 100 + 1);
-		for (var i = 0; i < already_used.length; i++) {
-			if (already_used[i] == new_num) {return}
-		};
-
-		parag.innerHTML += new_num+', ';
-		already_used.push(new_num);
-	}
-}
-
-var parag = document.getElementsByClassName('button12-1');
-var but=document.getElementsByClassName('button12');
-but.addEventListener('click', f12);
