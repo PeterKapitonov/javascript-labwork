87a14d71eb29b648cd33ed29cee1ec3ec01184ca
PeterKapitonov <79373925842@mail.ru>
2018-05-06 20:03:48 +0200

    lesson 37.jQuery

--- script.js
+++ script.js
@@ -1,67 +1,97 @@
-
 function task1(){
-	let newArr = ['первый', 'второй', 'третий', 'четвертый', 'пятый', 'шестой', 'седьмой'];
-	let [elem1, elem2, ...arr] = newArr;
-	document.write(elem1 + '| '+ elem2 + '|' +arr); 
-} 
+	var $h2=$('#task1 h2');
+	$h2.html('!');
+}
 
 function task2(){
-	let newArr = ['первый', 'второй', 'третий', 'четвертый', 'пятый', 'шестой', 'седьмой'];
-	newArr.reverse();
-	let [elem1, elem2] = newArr;
-	document.write(elem1 + '|'+ elem2);
-} 
-
+	var $h3=$('#task2 h3');
+	$h3.css({color:'red'});
+}
 
 function task3(){
-	let newArr = ['первый', 'второй', 'третий', 'четвертый', 'пятый', 'шестой', 'седьмой'];
-	let [, elem2] = newArr;
-	alert(elem2);  
-
-} 
+	var $h2=$('#task3 h2');
+	$h2.css({color:'blue',fontSize:'30px'});
+}
 
 function task4(){
-	let newArr = ['первый', 'второй'];
-	let [, elem2 = 'bbb', elem3 = 'eee'] = newArr;
-	document.write(elem2+' '+elem3); 
-
-} 
+	var $test=$('.test4');
+	$test.css({color:'red'});
+}
 
 function task5(){
-	let user = {
-		name: 'Пётр',
-		surname: 'Петров',
-		age: '20 лет'
-	};
-
-	let{name, surname, age} = user;
-	document.write(name + ' ' + surname + ' ' + age);  
-} 
+	var $test=$('h2.test5');
+	$test.css({color:'red'});
+}
 
 function task6(){
-	let user = {
-		//name: 'Пётр',
-		surname: 'Петров',
-		//age: '20 лет'
-	};
+	$('h3 + h3').css('color', 'red');
 
-	let{name = 'Аноним', surname = 'Анонимович', age = '? лет'} = user;
-	document.write(name + ' ' + surname + ' ' + age); 
-} 
-
+}
 
 function task7(){
-	let arr = ['elem1', 'elem2', 'elem3'];
-	for (let curr of arr){
-		alert(curr);
-	}
+	$('li + li.www').css('color', 'red');
+
 }
 
 function task8(){
-	let arr = ['elem1', 'elem2', 'elem3'];
-	arr.reverse();
-	for (let curr of arr){
-		alert(curr);
-	} 
+	$('h2 i').css('color', 'red');
+
 }
 
+function task8_1(){
+	$('h2 a').css('color', 'red');
+
+}
+
+function task8_2(){
+	$('h2 i').css({'color': 'black'});
+	$('h2 a').css({'color': 'black'});
+}
+
+function task9(){
+	$('#task9 h2>i').css('color', 'red');
+}
+
+function task10(){
+	$(':header').css('color', 'red');
+}
+
+function task11(){
+	$('h2:not(.test)').css('color', 'red');
+}
+
+function task12(){
+	$(':header:not(.test)').css('color', 'red');
+}
+
+function task13(){
+	$(':header + :header').css('color', 'red');
+}
+
+function task14(){
+	$(':header.test + :header.test').css('color', 'red');
+}
+
+function task15(){
+	$('#task15 div:empty').css('border', '1px solid red');
+}
+
+function task16(){
+	$('#task16 p:first').css('color', 'red');
+}
+
+function task17(){
+	$('#task17 p:last').css('color', 'red');
+}
+
+function task18(){
+	$('#task18 li:contains("Пункт")').css('color', 'red');
+}
+
+function task19(){
+	$('#task19 h2:has(i)').css('color', 'red');
+}
+
+function task20(){
+	$('#task20 h2>i').css('color', 'red');
+}
