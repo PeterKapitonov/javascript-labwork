2669eaff2222d5a2ef33c91179fc5911a63c92ea
PeterKapitonov <79373925842@mail.ru>
2018-05-06 20:00:41 +0200

    Урок 32.Задачи на продвинутую работу с функциями JavaScript

--- script.js
+++ script.js
@@ -1,60 +1,166 @@
-		var table = document.getElementById('tableSort');
-		var sortType = 1; //1 - по возрастанию, 0 - по убыванию
-		var classArrays = {
-			0: 'uarr',
-			1: 'darr',
+
+function task1(){
+	var a=prompt();
+	if (a > 0) {
+		var ggg = function() {
+			alert('!');
 		}
-		sortByTh(table.getElementsByTagName('th')[0]);
-		table.addEventListener('click', sortStart);
-		function sortStart(event) {
-			var targ = event.target;
-			var check = event.tagName
-			if (targ.tagName !== 'TH') return;
-			sortByTh(targ);
+	} else {
+		var ggg = function() {
+			alert('!!');
 		}
-		function sortByTh(th) {
-			var rows = getArray(table.tBodies[0].rows);
-			var index = th.cellIndex;
-			sortRows(rows, index);
-			sortType = -sortType + 1; 
-			addArrow(th);
-			printSort(rows);
+	}
+	ggg();	
+} 
+
+function task2(){
+	var ggg = function(n, f) {return f(n);}
+	var funcNew = function(a){return Math.pow(a, 2)};
+	num = 2;
+	alert(ggg(num, funcNew));
+} 
+
+
+function task3(){
+	var ggg = function(a, b){return a() + b();};
+	var f1 = function(){
+		return 3
+	}
+	var f2 = function(){
+		return 4
+	}
+	alert(ggg(f1, f2));
+
+} 
+
+function task4(){
+	var ggg = function(a, b, c){return b(a) + c(a);};
+	var square = function(n){return Math.pow(n, 2)};
+	var cub = function(n){return Math.pow(n, 3)};
+	alert(ggg(3, square, cub));
+
+} 
+
+function task5(){
+	var each = function(startArr, f){return f(startArr)};
+	var arr = [64, 49, 36, 25, 16];
+	var myFunc = function(a){
+		var newArr = [];
+		for (var i = 0; i < a.length; i++) {
+			newArr[i]=Math.sqrt(a[i]);
 		}
-		function addArrow(th) {
-			var allTh = table.querySelectorAll('th');
-			for (var i = 0; i < allTh.length; i++) {
-			   allTh[i].classList.remove(classArrays[0]);
-			   allTh[i].classList.remove(classArrays[1]);
-			}
-			th.classList.add(classArrays[sortType]);
-		}
-		function sortRows(rows, index) {
-			rows.sort(sort);
-			function sort(a, b) {
-				var val_a = a.children[index].innerHTML;
-				var val_b = b.children[index].innerHTML;
-				if (parseFloat(val_a)) {
-					val_a = parseFloat(val_a);
-					val_b = parseFloat(val_b);
-				}
-				if (sortType) {
-					return val_a > val_b;
-				} else {
-					return val_b > val_a;
-				}
-			}
-		}
-		function printSort(rows) {
-			var tbody = table.tBodies[0];
-			tbody.innerHTML = '';
-			for (var i = 0; i < rows.length; i++) {
-				tbody.appendChild(rows[i]);
-			}
-		}
-		function getArray(collection) {
-			var arr = [];
-			for (var i = 0; i < collection.length; i++) {
-				arr.push(collection[i]);
-			}
-			return arr;
-		}
+			return newArr;
+	}	
+
+	alert(each(arr, myFunc));
+} 
+
+function task6(){
+	var each = function(numArr, funcArray) {
+		var newArr = [];
+		for (var i = 0; i < numArr.length; i++){
+			var current = funcArray[i % funcArray.length](numArr[i]);
+			newArr.push(current);
+  		}
+		return newArr
+	}
+
+	var f1 = function(a){return a + 3};
+	var f2 = function(a){return a + 2};
+	var f3 = function(a){return a + 1};
+	var funcAll = [f1, f2, f3];
+	var startArray = [0,1,2, 3, 4, 5, 6, 7, 8, 9,10, 11];
+	document.write(each(startArray, funcAll));
+} 
+
+
+function task7(){
+	function counter() {
+		var i = 1;
+		return function() {return i++};
+	}
+
+	var func = counter();
+
+	alert(func());
+	alert(func());
+	alert(func());
+	alert(func());
+	alert(func());
+	alert(func());
+}
+
+var f1 = function(){   //task8 ----------------------------------------
+	var k = 1;
+	return function() {
+		this.innerHTML = k;
+		return k++;
+	}
+}
+
+var all_buttons = document.getElementsByClassName('button8');
+for (var i = 0; i < all_buttons.length; i++) {
+	all_buttons[i].addEventListener('click', f1(this));
+}
+
+
+
+
+//task9 ----------------------------------------
+
+
+var colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+var p_collect = document.getElementsByTagName('p');
+
+var f1 = function() {
+	var k = 0;
+	return function() {
+		this.style.color = colors[k];
+		k++;
+		if (k == colors.length){k=0};
+	}
+}
+
+for (var i = 0; i < p_collect.length; i++) {
+	p_collect[i].addEventListener('click', f1());
+}
+
+
+
+//task10  ----------------------------------------
+
+var f1 = function(){
+	var a = 0;
+	var b = 1;
+	var temp;
+	return function() {
+		this.nextElementSibling.innerHTML = a;
+		temp = a;
+		a = b;
+		b = temp +a;
+	}
+}
+
+var buts_collect = document.getElementsByClassName('button10');
+for (var i = 0; i < buts_collect.length; i++) {
+	buts_collect[i].addEventListener('click', f1());
+}
+
+//task12 ----------------------------------------
+
+var f12 = function() {
+	var already_used = [];
+	return function() {
+		var new_num = Math.floor(Math.random() * 100 + 1);
+		for (var i = 0; i < already_used.length; i++) {
+			if (already_used[i] == new_num) {return}
+		};
+
+		parag.innerHTML += new_num+', ';
+		already_used.push(new_num);
+	}
+}
+
+var parag = document.getElementsByClassName('button12-1');
+var but=document.getElementsByClassName('button12');
+but.addEventListener('click', f12);
