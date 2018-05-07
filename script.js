20afc1729e0c13b35b57a943fcde26a5cf57f856
PeterKapitonov <79373925842@mail.ru>
2018-05-07 21:14:00 +0200

    lesson 39

--- script.js
+++ script.js
@@ -1,68 +1,43 @@
-function task1(){
-	var $a=$('li').not('.test1');
-	$a.css({color:'blue'});
-}
+$('#task1 p').click(function() {   //task1
+	alert($(this).html());
+});
 
+$('#task2 a').mouseover(task2); //task2
 function task2(){
-	alert($('#task2 li').first().html());
-}
+	$(this).html($(this).html()+'('+$(this).attr('href')+')');
+};
 
+$('#task3 a').mouseover(task3); //task3
 function task3(){
-	alert($('#task3 li').last().html());
-}
+	$(this).html($(this).html()+'('+$(this).attr('href')+')');
+	$(this).off('mouseover', task3);
+};
 
-function task4(){
-	var $a=$('#task4 li').eq(4);
-	alert($a.html());
-}
+$('#task4 input').mouseover(function(){ //task4
+	$('#test4').html($('#test4').html()+' '+$(this).attr('value'));
+	
+});
 
-function task5(){
-	var $test=$('#task5 li').eq(-2);
-	alert($test.html());
-}
+$('#task5 input').click(function(){ //task5
+	alert($(this).attr('value'));
+	$(this).off('click');
+});
 
-function task6(){
-	var $test=$('#task6 li').slice(1, 4);
-	$test.css('color', 'red');
+$('#task6 p').click(function(){ //task6
+	alert($(this).html()**2);
+});
 
-}
+$('#task7 p').click(function(){ //task6
+	$(this).html($(this).html()**2);
+	$(this).off('click');
+});
 
-function task7(){
-	$('li').filter('.test7').css('color', 'red');
-}
+$('#task7 p').dblclick(function(){ 
+	$(this).html(String($(this).html())+String($(this).html()));
+	$(this).off('dblclick');
+});
 
+$('#task8 a').mouseover(task8); //task2
 function task8(){
-	$('.test8').next().css('color', 'red');
-
-}
-
-
-
-function task9(){
-	$('.test9').prev().css('color', 'red');
-}
-
-function task10(){
-	$('.test10').nextAll().css('color', 'red');
-}
-
-function task11(){
-	$('.test11').prevAll().css('color', 'red');
-}
-
-function task12(){
-	$('.www').nextUntil('.test12').css('color', 'red');
-}
-
-function task13(){
-	$('.test13').prevUntil('.www').css('color', 'red');
-}
-
-function task14(){
-	$('h1.test14').siblings().css('color', 'red');
-}
-
-function task15(){
-	$('.test15').siblings('.test').css('color', 'red');
-}
-
+	$(this).html($(this).html()+'('+$(this).attr('href')+')');
+};
